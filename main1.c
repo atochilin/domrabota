d21f1b749803ccdbf35821ec853d568ecc2dfcb4
atochilin <andr.tochilin2003@yandex.ru>
2021-09-24 18:15:32 +0300

    add fail

---
+++ main1.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+ 
+int main () 
+{
+    int a, b = 2, t; // t - один остаток;
+    int n = 0; // количество остатков;
+    int r[200], k = 0; // k - это также количество остатков,
+                       // мы будем использовать его, чтобы показать количество элементов во втором массиве;
+    scanf ("%d", &a);
+    int D = a;    
+    while (a > 0)
+    {
+        t = a % b;
+        k = n;
+        r[k] = t;
+        n += 1;
+        a = a / b;
+//        printf ("%d", t);
+    }
+    
+    printf ("The number of remnants is %d", n);
+    printf ("\n");
+    printf ("The binary number of %d is ", D);
+    for (int i = k-1; k >= 0; k--)
+    {
+        printf ("%d", r[k]);
+    }
+    return 0;
+}
